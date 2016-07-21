064e4f64b9251645d798f2b0c53221f851272971
pawanake <382729098@qq.com>
2016-07-21 14:46:36 +0800

    update: 1.add laya engine 1.0.2

--- generators/app/index.js
+++ generators/app/index.js
@@ -27,7 +27,7 @@ module.exports = yeoman.Base.extend({
 		},{
 			type: 'list',
 			name: 'engineversion',
-			choices: [ '1.0.0','0.9.9','0.9.8', '0.9.7' ],
+			choices: [ '1.0.2','1.0.0','0.9.9','0.9.8', '0.9.7' ],
 			message: 'Which version of LayaAir would you like to use ?'
 		}];
 		
@@ -69,6 +69,6 @@ module.exports = yeoman.Base.extend({
 	},
 
 	install: function () {
-		//this.npmInstall()
+		this.npmInstall()
 	}
 });
