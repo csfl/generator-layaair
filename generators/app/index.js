3845c13467b26ebf08e5f3595bd98ff5b2d86524
pawanake <382729098@qq.com>
2016-12-29 17:04:39 +0800

    1.修正了升级依赖库导致的不可用问题，版本号0.2.2

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var baseGenerator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var path = require('path');
@@ -19,99 +19,115 @@ var fetchdata = function( url ) {
 	})
 }
 
-module.exports = yeoman.Base.extend({
+var defaultPrompts = function() {
+	var dirname = path.relative( process.cwd(), path.basename( process.cwd() ) )
+	return [{
+		type: 'input',
+		name: 'name',
+		message: 'First off, how would you like to name this project ?',
+		default: dirname
+	},{
+		type: 'list',
+		name: 'language',
+		choices: [ 'typescript', 'javascript' ],
+		message: 'Which language would you like to use ?'
+	},{
+		type: 'list',
+		name: 'engineversion',
+		message: 'what version of LayaAir would you like to use ?',
+		choices: []
+	}];
+}
 
-	prompting: function () {
-		var self = this;
+module.exports = class extends baseGenerator {
+	constructor(args, opts) {
+		super(args, opts);
+	}
+
+	prompting() {
 		var done = this.async();
-
-		var dirname = path.relative( process.cwd(), path.basename( process.cwd() ) )
-		var prompts = [{
-			type: 'input',
-			name: 'name',
-			message: 'First off, how would you like to name this project ?',
-			default: dirname
-		},{
-			type: 'list',
-			name: 'language',
-			choices: [ 'typescript', 'javascript' ],
-			message: 'Which language would you like to use ?'
-		},{
-			type: 'list',
-			name: 'engineversion',
-			message: 'what version of LayaAir would you like to use ?',
-			choices: []
-		}];
-
 		this.log(yosay(
 			'Welcome to ' + chalk.red('LayaAir') + ' generator!'
 		));
 
 		fetchdata('http://ldc.layabox.com/index.php?m=content&c=index&a=lists&catid=28')
 		.then(function(data){
+			var prompts = defaultPrompts();
 			var $ = cheerio.load(data)
 			$('.version-list .version').map(function(i){return $(this).text()}).get().forEach(function(item){
 				if( item.match(/^\d+\.\d+\.\d+$/) ) {
 					prompts[2].choices.push(item)
 				}
 			})
-			self.prompt(prompts, function (props) {
-				this.props = props;
-				done();
-			}.bind(self));
-		})
+			return this.prompt(prompts)
+		}.bind(this))
+		.then(function(props){
+			this.props = props;
+			done();
+		}.bind(this))
+		.error(function(err){
+			done(err);
+		}.bind(this))
+	}
 
-	},
-
-	writing: function () {
-		console.log('   generating config.js')
+	writing() {
+		this.log('   generating config.js')
 		this.fs.write(this.destinationPath('config.js'),
 			"module.exports = {\n" +
-			"\tenableTypings: false,\n" +
+			(this.props.language === 'typescript' ? "\tenableTypings: false,\n" : "") +
 			"\tlayaModules: ['laya.core','laya.webgl','laya.html','laya.ui','laya.debugtool','matter','matter-RenderLaya'],\n" +
 			"\tlayaVersion: '" + this.props.engineversion + "'\n" +
 			"}"
 		)
-
-		if( this.props.language == 'typescript' ) {
-			console.log('   generating package.json')
-			var packageinfo = this.fs.readJSON( this.templatePath('tsfiles/package.json') );
-			packageinfo.name = this.props.name;
-			this.fs.extendJSON( 'package.json',	packageinfo, null, '\t' )
-			
-			console.log('   copying other config files')
-			var rootfiles = ['gulpfile.js','tsconfig.json','typings.json'];
-			for( var i = 0 ; i < rootfiles.length ; i++ ) {
-				this.fs.copy( this.templatePath( 'tsfiles/' + rootfiles[i]), this.destinationPath(rootfiles[i]) );
-			}
-
-			console.log('   copying template files')
-			var subDirs = [ 'res','tsfiles/src','template', 'tsfiles/scripts' ];
-			for( var i = 0 ; i < subDirs.length ; i++ ) {
-				this.fs.copy( this.templatePath( subDirs[i] + '/**' ), this.destinationPath( subDirs[i].replace('tsfiles/','') )	);
-			}
+		switch( this.props.language ) {
+			case 'typescript':
+				this._writeTs()
+				break;
+			case 'javascript':
+				this._writeJs()
+				break;
 		}
-		else if( this.props.language == 'javascript' ) {
-			console.log('   generating package.json')
-			var packageinfo = this.fs.readJSON( this.templatePath('jsfiles/package.json') );
-			packageinfo.name = this.props.name;
-			this.fs.extendJSON( 'package.json',	packageinfo, null, '\t' )
+	}
 
-			console.log('   copying other config files')
-			var rootfiles = [ 'gulpfile.js','webpack.config.js' ];
-			for( var i = 0 ; i < rootfiles.length ; i++ ) {
-				this.fs.copy( this.templatePath( 'jsfiles/' + rootfiles[i]), this.destinationPath(rootfiles[i]) );
-			}
+	_writeTs() {
+		this.log('   generating package.json')
+		var packageinfo = this.fs.readJSON( this.templatePath('tsfiles/package.json') );
+		packageinfo.name = this.props.name;
+		this.fs.extendJSON( 'package.json',	packageinfo, null, '\t' )
 
-			console.log('   copying template files')
-			var subDirs = [ 'res','jsfiles/src','template', 'jsfiles/scripts' ];
-			for( var i = 0 ; i < subDirs.length ; i++ ) {
-				this.fs.copy( this.templatePath( subDirs[i] + '/**' ), this.destinationPath( subDirs[i].replace('jsfiles/','') )	);
-			}
+		this.log('   copying other config files')
+		var rootfiles = ['gulpfile.js','tsconfig.json','typings.json'];
+		for( var i = 0 ; i < rootfiles.length ; i++ ) {
+			this.fs.copy( this.templatePath( 'tsfiles/' + rootfiles[i]), this.destinationPath(rootfiles[i]) );
 		}
-	},
 
-	install: function () {
+		this.log('   copying template files')
+		var subDirs = [ 'res','tsfiles/src','template', 'tsfiles/scripts' ];
+		for( var i = 0 ; i < subDirs.length ; i++ ) {
+			this.fs.copy( this.templatePath( subDirs[i] + '/**' ), this.destinationPath( subDirs[i].replace('tsfiles/','') )	);
+		}
+	}
+
+	_writeJs() {
+		this.log('   generating package.json')
+		var packageinfo = this.fs.readJSON( this.templatePath('jsfiles/package.json') );
+		packageinfo.name = this.props.name;
+		this.fs.extendJSON( 'package.json',	packageinfo, null, '\t' )
+
+		this.log('   copying other config files')
+		var rootfiles = [ 'gulpfile.js','webpack.config.js' ];
+		for( var i = 0 ; i < rootfiles.length ; i++ ) {
+			this.fs.copy( this.templatePath( 'jsfiles/' + rootfiles[i]), this.destinationPath(rootfiles[i]) );
+		}
+
+		this.log('   copying template files')
+		var subDirs = [ 'res','jsfiles/src','template', 'jsfiles/scripts' ];
+		for( var i = 0 ; i < subDirs.length ; i++ ) {
+			this.fs.copy( this.templatePath( subDirs[i] + '/**' ), this.destinationPath( subDirs[i].replace('jsfiles/','') )	);
+		}
+	}
+
+	install() {
 		this.npmInstall()
 	}
-});
+};
